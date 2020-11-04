c54130ed4789c7948f07b2f4558c000465c1e3e1
Ehimeh <65470921+Ehimeh@users.noreply.github.com>
2020-11-04 12:39:11 +0100

    Add files via upload

---
+++ POPBOOK/assets/js/ex.js
@@ -0,0 +1,9 @@
+// Number(prompt('input number'))
+// Number(prompt('input number'))
+// var number= num1 + num2
+
+
+var firstNumber = Number(prompt("input firstnumber"));
+var secondNumber = Number(prompt("input secondnumber"));
+
+var answer 
